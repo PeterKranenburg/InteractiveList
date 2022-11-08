a0812c42c7303bc70421a933b1ec69c69f3b6bf8
Elise <justin555@live.nl>
2022-11-08 14:11:47 +0100

    Updated Json data and fixed issue #4

--- js/ZorgTools.js
+++ js/ZorgTools.js
@@ -8,51 +8,51 @@ var zorgToolData = [
  {
   "Toolnaam": "Domotica",
   "Toolbeschrijving": "Waarbij het bij ‘gewone’ domotica draait om het automatiseren van apparatuur thuis of in een ander gebouw, spreekt men van een specifieke zorgdomotica wanneer de geautomatiseerde toepassingen worden gebruik om het leven van hulpbehoevenden of ouderen gemakkelijker te maken. Hierbij moet je ook weer denken aan geautomatiseerde toepassingen binnen een huiselijke omgeving. Dit kan binnen een aangepast woonhuis als binnen een woon-zorginstelling.",
-  "Toollink": "Zorgdomotica | Wat is het? | Verduurzaming Nederland"
+  "Toollink": "https:\/\/www.ablebits.com\/office-addins-blog\/hyperlink-excel-create-edit-remove\/"
  },
  {
   "Toolnaam": "MomoBedsense",
   "Toolbeschrijving": "De Momo BedSense is een sensorplaat die op borsthoogte onder het matras van de bewoner wordt geplaatst. De sensoren geven informatie over druk, houding en trillingen. Deze data wordt weergegeven in een app die inzicht geeft in welke bewoners veilig en rustig in bed liggen, wie onrustig is en wie dringend hulp nodig heeft om bijvoorbeeld valincidenten of decubitus te voorkomen. Zorgmedewerkers kunnen zo goed inschatten welke acties nodig zijn. De beschikbare informatie in de app kan tevens door behandelaren worden geanalyseerd voor gerichte advisering over interventies, bijvoorbeeld om het slaappatroon te bevorderen",
-  "Toollink": "Momo Medical | Home"
+  "Toollink": "https:\/\/momomedical.nl\/"
  },
  {
   "Toolnaam": "Persoonsalarmering",
-  "Toolbeschrijving": "Wat is personenalarmering? Personenalarmering is een veiligheidssysteem. Het bestaat uit een draadloze zender en een alarmapparaat. De zender draagt u om uw hals of pols. Zo heeft u deze altijd bij de hand. De zender heeft een bereik van ongeveer 30 meter. Hij hoeft niet te worden opgeladen.",
-  "Toollink": "Wat is personenalarmering? | Onbezorgdthuiswonen.nl - Altijd hulp binnen handbereik"
+  "Toolbeschrijving": "Wat is personenalarmering? Personenalarmering is een&nbsp;veiligheidssysteem. Het bestaat uit een draadloze zender en een alarmapparaat. De zender draagt u om uw hals of pols. Zo heeft u deze altijd bij de hand. De zender heeft een bereik van ongeveer 30 meter. Hij hoeft niet te worden opgeladen.",
+  "Toollink": "http:\/\/www.onbezorgdthuiswonen.nl\/wat-is-personenalarmering - :~:text=Wat%20is%20personenalarmering%3F%20Personenalarmering%20is%20een%20veiligheidssysteem.%20Het,30%20meter.%20Hij%20hoeft%20niet%20te%20worden%20opgeladen."
  },
  {
   "Toolnaam": "Medido",
-  "Toolbeschrijving": "Versterk de zelfredzaamheid van cliënten met Medido. De automatische medicijndispenser biedt op het juiste tijdstip de juiste medicatie aan. Zo is de cliënt niet langer afhankelijk van de thuiszorg voor het aanreiken van de medicatie.",
-  "Toollink": "Automatische medicijndispenser Medido"
+  "Toolbeschrijving": "Versterk de zelfredzaamheid van cliënten met Medido. De automatische medicijndispenser biedt op het juiste tijdstip de juiste medicatie aan. Zo is de cliënt niet langer afhankelijk van de thuiszorg voor het&nbsp;aanreiken van de medicatie.",
+  "Toollink": "https:\/\/medido.com\/nl"
  },
  {
   "Toolnaam": "Slimme sensoren",
-  "Toolbeschrijving": "Er zijn verschillende soorten sensoren die monitoren waar iemand heen gaat, naar welke kamer en hoe lang diegene daar is. Deze vorm van personenalarmering, ofwel domotica, ‘weet’ wanneer iemand plotseling niet meer beweegt, valt of is vergeten te eten. De zorgverleners krijgen dan een noodmelding. De sensoren kunnen op verschillende plaatsen in de woning of zorginstelling worden geïnstalleerd. ",
-  "Toollink": "Slimme domotica - YouTube"
+  "Toolbeschrijving": "Er zijn verschillende soorten sensoren die monitoren waar iemand heen gaat, naar welke kamer en hoe lang diegene daar is. Deze vorm van personenalarmering, ofwel domotica, ‘weet’ wanneer iemand plotseling niet meer beweegt, valt of is vergeten te eten. De zorgverleners krijgen dan een noodmelding. De sensoren kunnen op verschillende plaatsen in de woning of zorginstelling worden geïnstalleerd.&nbsp;",
+  "Toollink": "https:\/\/www.youtube.com\/watch?v=ya1xMTajxC4&amp;t=3s"
  },
  {
   "Toolnaam": "slimme camera's",
   "Toolbeschrijving": "Valpartijen die niet op tijd ontdekt worden zijn een groot gezondheidsrisico voor ouderen. Met de huidige domotica of slimme sensoren, is in 6 van de 7 valpartijen iemand na 5 minuten pas aanwezig om de cliënt te helpen. Daarnaast wordt het zorgpersoneel overspoeld met valse alarmen.",
-  "Toollink": "Ontdek de Kepler Night Nurse | Kepler Vision Technologies"
+  "Toollink": "https:\/\/keplervision.eu\/night-nurse\/"
  },
  {
   "Toolnaam": "Smart floor, valdetectie",
-  "Toolbeschrijving": "Door bewegingsgedrag van ouderen automatisch (zonder aanwezigheid van personeel) periodiek te meten met een loopsensor in samenwerking met een slimme vloer (vijf tot tien meter), wordt direct duidelijk hoe hoog het valrisico is. De valrisico thermometer in de Smart Floor App (groen is veilig, bij oranje is het oppassen, bij rood rinkelen de alarmbellen) geeft aan of extra fysiotherapie kan helpen om beter en stabieler te bewegen of dat er een loophulpmiddel moet worden ingezet. Het kan ook zijn dat het valrisico  zo groot is dat een heupairbag (in combinatie met een loophulpmiddel) de aangewezen interventie is.",
-  "Toollink": "Smart Floor – Anders werken in de zorg"
+  "Toolbeschrijving": "Door bewegingsgedrag van ouderen automatisch (zonder aanwezigheid van personeel) periodiek te meten met een loopsensor in samenwerking met een slimme vloer (vijf tot tien meter), wordt direct duidelijk hoe hoog het valrisico is. De valrisico thermometer in de Smart Floor App (groen is veilig, bij oranje is het oppassen, bij rood rinkelen de alarmbellen) geeft aan of extra fysiotherapie kan helpen om beter en stabieler te bewegen of dat er een loophulpmiddel moet worden ingezet. Het kan ook zijn dat het valrisico&nbsp; zo groot is dat een heupairbag (in combinatie met een loophulpmiddel) de aangewezen interventie is.",
+  "Toollink": "https:\/\/anderswerkenindezorg.nl\/innovatie\/smart-floor\/"
  },
  {
   "Toolnaam": "Wolk heupairbag valdetectie",
-  "Toolbeschrijving": "De Wolk heupairbag voorkomt heupfracturen bij een val. Wolk is een compacte, herbruikbare heupairbag die ouderen gemakkelijk kunnen dragen. Sensoren in de Wolk detecteren een val, de airbag blaast zichzelf razendsnel op en de heupen zijn beschermd.",
-  "Toollink": "Wolk – Anders werken in de zorg"
+  "Toolbeschrijving": "De&nbsp;Wolk heupairbag&nbsp;voorkomt heupfracturen bij een val. Wolk is een compacte, herbruikbare heupairbag die ouderen gemakkelijk kunnen dragen. Sensoren in de Wolk detecteren een val, de airbag blaast zichzelf razendsnel op en de heupen zijn beschermd.",
+  "Toollink": "https:\/\/anderswerkenindezorg.nl\/innovatie\/wolk\/"
  },
  {
   "Toolnaam": "slim incontinetiemateriaal",
-  "Toolbeschrijving": "De baanbrekende en betrouwbare zorgsoftware van Kepler Night Nurse elimineert 99% van de valse alarmen en heeft nog vele andere voordelen.",
-  "Toollink": "Slim incontinentiemateriaal – Anders werken in de zorg"
+  "Toolbeschrijving": "De baanbrekende en betrouwbare zorgsoftware van Kepler Night Nurse elimineert&nbsp;99%&nbsp;van de valse alarmen&nbsp;en heeft nog vele andere voordelen.",
+  "Toollink": "https:\/\/anderswerkenindezorg.nl\/innovatie\/slim-incontinentiemateriaal\/"
  },
  {
   "Toolnaam": "smart glasses",
-  "Toolbeschrijving": "Met de Smart Glass kan de zorgmedewerker door middel van een camera direct contact leggen met een verpleegkundige of arts. De verpleegkundige of arts kan meteen door de ogen van de medewerker meekijken en ondersteuning bieden. Deskundigheid zónder afstand. De voordelen voor de cliënt: specialistische zorg is snel en dichtbij geregeld. Afstand en reistijd is geen bezwaar. De cliënt is omringd door vertrouwde gezichten en de cliënt kan meepraten en vragen stellen aan de deskundige. ",
-  "Toollink": "1 Minuut – Innovatie samen met de zorg!"
+  "Toolbeschrijving": "Met de Smart Glass kan de zorgmedewerker door middel van een camera direct contact leggen met een verpleegkundige of arts. De verpleegkundige of arts kan meteen door de ogen van de medewerker meekijken en ondersteuning bieden. Deskundigheid zónder afstand. De voordelen voor de cliënt: specialistische zorg is snel en dichtbij geregeld. Afstand en reistijd is geen bezwaar. De cliënt is omringd door vertrouwde gezichten en de cliënt kan meepraten en vragen stellen aan de deskundige.&nbsp;",
+  "Toollink": "https:\/\/1minuut.com\/"
  }
 ]
