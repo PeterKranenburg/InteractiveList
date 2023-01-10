88d02740a980187f972d54006d4ab81e7da95cb6
PeterKranenburg <118988924+PeterKranenburg@users.noreply.github.com>
2023-01-10 14:24:03 +0100

    Update ZorgTools.js

--- js/ZorgTools.js
+++ js/ZorgTools.js
@@ -1,14 +1,14 @@
 var zorgToolData = [
  {
-  "Toolnaam": "E-health:1 Beeldbellen, ECD Electronisch clienten dossier",
-  "Toolbeschrijving": "We leven allemaal tegen woordig digitaal dus waarom niet bellen met je dokter. Of inzien hoe het met je gaat. Dat allemaal digitaal bij de hand.",
+  "Toolnaam": "E-health:Beeldbellen, ECD Electronisch clienten dossier",
+  "Toolbeschrijving": "We leven allemaal tegenwoordig digitaal dus waarom niet bellen met je dokter. Of inzien hoe het met je gaat. Dat allemaal digitaal bij de hand.",
   "Toolimg": "https:\/\/www.ictmagazine.nl\/wp-content\/uploads\/2017\/06\/Cormel.png",
   "Toollink": "Null"
  },
  {
   "Toolnaam": "Domotica",
   "Toolbeschrijving": "Waarbij het bij ‘gewone’ domotica draait om het automatiseren van apparatuur thuis of in een ander gebouw, spreekt men van een specifieke zorgdomotica wanneer de geautomatiseerde toepassingen worden gebruik om het leven van hulpbehoevenden of ouderen gemakkelijker te maken. Hierbij moet je ook weer denken aan geautomatiseerde toepassingen binnen een huiselijke omgeving. Dit kan binnen een aangepast woonhuis als binnen een woon-zorginstelling.",
-  "Toolimg": "Cosè-la-domotica.jpg (1920×1280) (ombregialle.it)",
+  "Toolimg": "https:\/\/ombregialle.it\/wp-content\/uploads\/2020\/02\/Cos%C3%A8-la-domotica.jpg",
   "Toollink": "https:\/\/www.verduurzamingnederland.nl\/zorgdomotica"
  },
  {
@@ -26,43 +26,46 @@ var zorgToolData = [
  {
   "Toolnaam": "Medido",
   "Toolbeschrijving": "Versterk de zelfredzaamheid van cliënten met Medido. De automatische medicijndispenser biedt op het juiste tijdstip de juiste medicatie aan. Zo is de cliënt niet langer afhankelijk van de thuiszorg voor het&nbsp;aanreiken van de medicatie.",
-  "Toolimg": "Het-begin-van-de-aurotmatishe-medicijn-4ad97769.webp (1200×628) (medido.com)",
+  "Toolimg": "https:\/\/medido.com\/templates\/yootheme\/cache\/4a\/Het-begin-van-de-aurotmatishe-medicijn-4ad97769.webp",
   "Toollink": "https:\/\/medido.com\/nl"
  },
  {
   "Toolnaam": "Slimme sensoren",
   "Toolbeschrijving": "Er zijn verschillende soorten sensoren die monitoren waar iemand heen gaat, naar welke kamer en hoe lang diegene daar is. Deze vorm van personenalarmering, ofwel domotica, ‘weet’ wanneer iemand plotseling niet meer beweegt, valt of is vergeten te eten. De zorgverleners krijgen dan een noodmelding. De sensoren kunnen op verschillende plaatsen in de woning of zorginstelling worden geïnstalleerd.&nbsp;",
-  "Toolimg": "OIP.FSLSOylm_qAAp6OYRmdSIwAAAA (325×215) (bing.com)",
+  "Toolimg": "https:\/\/th.bing.com\/th\/id\/OIP.FSLSOylm_qAAp6OYRmdSIwAAAA?pid=ImgDet&amp;rs=1",
   "Toollink": "https:\/\/www.youtube.com\/watch?v=ya1xMTajxC4&amp;t=3s"
  },
  {
   "Toolnaam": "slimme camera's",
   "Toolbeschrijving": "Valpartijen die niet op tijd ontdekt worden zijn een groot gezondheidsrisico voor ouderen. Met de huidige domotica of slimme sensoren, is in 6 van de 7 valpartijen iemand na 5 minuten pas aanwezig om de cliënt te helpen. Daarnaast wordt het zorgpersoneel overspoeld met valse alarmen.",
-  "Toolimg": "website-uitgelichte-foto.jpg (2407×1936) (zetacom.nl)",
+  "Toolimg": "https:\/\/www.zetacom.nl\/wp-content\/uploads\/2018\/11\/website-uitgelichte-foto.jpg",
   "Toollink": "https:\/\/keplervision.eu\/night-nurse\/"
  },
  {
   "Toolnaam": "Smart floor, valdetectie",
   "Toolbeschrijving": "Door bewegingsgedrag van ouderen automatisch (zonder aanwezigheid van personeel) periodiek te meten met een loopsensor in samenwerking met een slimme vloer (vijf tot tien meter), wordt direct duidelijk hoe hoog het valrisico is. De valrisico thermometer in de Smart Floor App (groen is veilig, bij oranje is het oppassen, bij rood rinkelen de alarmbellen) geeft aan of extra fysiotherapie kan helpen om beter en stabieler te bewegen of dat er een loophulpmiddel moet worden ingezet. Het kan ook zijn dat het valrisico&nbsp; zo groot is dat een heupairbag (in combinatie met een loophulpmiddel) de aangewezen interventie is.",
-  "Toolimg": "case-empathischewoning.jpg (525×367) (smartfloor.com)",
+  "Toolimg": "https:\/\/smartfloor.com\/wp-content\/uploads\/2017\/05\/case-empathischewoning.jpg",
   "Toollink": "https:\/\/anderswerkenindezorg.nl\/innovatie\/smart-floor\/"
  },
  {
   "Toolnaam": "Wolk heupairbag valdetectie",
   "Toolbeschrijving": "De&nbsp;Wolk heupairbag&nbsp;voorkomt heupfracturen bij een val. Wolk is een compacte, herbruikbare heupairbag die ouderen gemakkelijk kunnen dragen. Sensoren in de Wolk detecteren een val, de airbag blaast zichzelf razendsnel op en de heupen zijn beschermd.",
-  "Toolimg": "header-wolk-heupairbag-belt-shorts.png (800×600) (wolkairbag.com)",
+  "Toolimg": "https:\/\/wolkairbag.com\/wp-content\/uploads\/2022\/07\/header-wolk-heupairbag-belt-shorts.png",
   "Toollink": "https:\/\/anderswerkenindezorg.nl\/innovatie\/wolk\/"
  },
  {
   "Toolnaam": "slim incontinetiemateriaal",
   "Toolbeschrijving": "De baanbrekende en betrouwbare zorgsoftware van Kepler Night Nurse elimineert&nbsp;99%&nbsp;van de valse alarmen&nbsp;en heeft nog vele andere voordelen.",
-  "Toolimg": "Visual1_700x500_v2.jpg (700×500) (nursing.nl)",
+  "Toolimg": "https:\/\/www.nursing.nl\/content\/uploads\/2021\/11\/Visual1_700x500_v2.jpg",
   "Toollink": "https:\/\/anderswerkenindezorg.nl\/innovatie\/slim-incontinentiemateriaal\/"
  },
  {
   "Toolnaam": "smart glasses",
   "Toolbeschrijving": "Met de Smart Glass kan de zorgmedewerker door middel van een camera direct contact leggen met een verpleegkundige of arts. De verpleegkundige of arts kan meteen door de ogen van de medewerker meekijken en ondersteuning bieden. Deskundigheid zónder afstand. De voordelen voor de cliënt: specialistische zorg is snel en dichtbij geregeld. Afstand en reistijd is geen bezwaar. De cliënt is omringd door vertrouwde gezichten en de cliënt kan meepraten en vragen stellen aan de deskundige.&nbsp;",
-  "Toolimg": "Mandy-Broeren-ZGEM-e1620743016262-768x279.png (768×279) (1minuut.com)",
+  "Toolimg": "https:\/\/1minuut.com\/wp-content\/uploads\/2021\/05\/Mandy-Broeren-ZGEM-e1620743016262-768x279.png",
   "Toollink": "https:\/\/1minuut.com\/"
+ },
+ {
+  "Toollink": "https:\/\/www.youtube.com\/watch?v=XLP4YTpUpBI"
  }
 ]
